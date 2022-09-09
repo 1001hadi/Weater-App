4f1fd927ba7e338196a2e99e2344b553942c3a5b
1001hadi <hadi.jaf.1001@gmail.com>
2022-09-08 19:57:36 -0600

    js file has been updated

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 class Weather {
     constructor() {
         this.key = "V6Z2he9kPWcX6zyKtQ0w5WSNUFhYGIIB";
-        this.WeatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.WeatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     }
     async updateCity(city) {
         const cityInfo = await this.getCity(city);
